ad7c4ac710607ab45174ec818ddd34a8e47e449d
Azeem <livelyazeem@gmail.com>
2022-08-11 12:55:09 +0500

    defined initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './style.scss';
 
-const calcButtons = (props) => {
-    return <div style={props.style}></div>
-}
+const CalcButton = (props) => {
+    return (
+        <button id="calcBtn" style={props.style} onClick={props.onClick}>
+            {props.btnText}
+        </button>
+    );
+};
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -12,7 +16,64 @@ class App extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div id="container">
+                <div id="display"></div>
+                <CalcButton
+                    style={{
+                        width: 150,
+                        backgroundColor: 'rgb(173, 0, 0)',
+                    }}
+                    btnText="AC"
+                />
+                <CalcButton
+                    btnText="/"
+                    style={{
+                        backgroundColor: 'gray',
+                    }}
+                />
+                <CalcButton
+                    btnText="X"
+                    style={{
+                        backgroundColor: 'gray',
+                    }}
+                />
+                <CalcButton btnText="7" />
+                <CalcButton btnText="8" />
+                <CalcButton btnText="9" />
+                <CalcButton
+                    btnText="-"
+                    style={{
+                        backgroundColor: 'gray',
+                    }}
+                />
+                <CalcButton btnText="4" />
+                <CalcButton btnText="5" />
+                <CalcButton btnText="6" />
+                <CalcButton
+                    btnText="+"
+                    style={{
+                        backgroundColor: 'gray',
+                    }}
+                />
+                <CalcButton btnText="1" />
+                <CalcButton btnText="2" />
+                <CalcButton btnText="3" />
+                <CalcButton
+                    btnText="="
+                    style={{
+                        float: 'right',
+                        height: 150,
+                        backgroundColor: 'rgb(34, 86, 134)',
+                    }}
+                />
+                <CalcButton
+                    btnText="0"
+                    style={{
+                        width: 150,
+                    }}
+                />
+                <CalcButton btnText="." />
+            </div>
         );
     }
 }
