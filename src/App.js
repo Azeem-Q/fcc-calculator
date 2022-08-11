f05ddfa09ba42d2f60bc1054ad39d5643f74a7f2
Azeem <livelyazeem@gmail.com>
2022-08-11 11:33:39 +0500

    changed name and title, initialize calcButtons component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './style.scss';
 
+const calcButtons = (props) => {
+    return <div style={props.style}></div>
+}
 class App extends React.Component {
     constructor(props) {
         super(props);
